ff878a3750585f774596827823d2ab18e12309a0
hellosooka <leo.zemtsoff@gmail.com>
2022-06-16 12:11:01 +0300

    add header in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
+<script>
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+
+  components: {
+    'the-header': TheHeader
+  }
+}
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <the-header> </the-header>
+
   <router-view/>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;600;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: Rubik;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+*, *::before, *::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
 }
+
 </style>
